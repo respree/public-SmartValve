21d4732ce5b993c23475a8737e64ee4e5f433d15
respree <respree@gmail.com>
2024-01-01 02:56:08 +0900

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -1,3 +1,4 @@
+wget https://raw.githubusercontent.com/respree/public-SmartValve/main/smartValve1230.tar.gz
 tar -xf smartValve1230.tar.gz
-rm smartValve
+rm -f smartValve
 ln -s smartValve1230 smartValve
